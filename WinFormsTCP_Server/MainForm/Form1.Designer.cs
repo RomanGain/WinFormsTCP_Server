feda2c24559064cfd0af044c727240cbed7ceda3
RomanGain <romangajnullin@gmail.com>
2016-08-19 15:05:47 +0300

    19/08/2016

---
+++ WinFormsTCP_Server/MainForm/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace WinFormsTCP_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnMessage = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnApplication = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblErrors = new System.Windows.Forms.Label();
+            this.lblExceptions = new System.Windows.Forms.Label();
+            this.lblErrorsValue = new System.Windows.Forms.Label();
+            this.lblExceptionsValue = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.фToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.параметрыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Image = global::WinFormsTCP_Server.Properties.Resources.door;
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnID,
+            this.columnMessage,
+            this.columnDate,
+            this.columnType,
+            this.columnApplication});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(742, 520);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnID
+            // 
+            this.columnID.Text = "ID";
+            this.columnID.Width = 47;
+            // 
+            // columnMessage
+            // 
+            this.columnMessage.Text = "Message";
+            this.columnMessage.Width = 302;
+            // 
+            // columnDate
+            // 
+            this.columnDate.Text = "Date";
+            this.columnDate.Width = 143;
+            // 
+            // columnType
+            // 
+            this.columnType.Text = "Type";
+            this.columnType.Width = 103;
+            // 
+            // columnApplication
+            // 
+            this.columnApplication.Text = "Application";
+            this.columnApplication.Width = 119;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 584);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblErrors
+            // 
+            this.lblErrors.AutoSize = true;
+            this.lblErrors.Location = new System.Drawing.Point(760, 42);
+            this.lblErrors.Name = "lblErrors";
+            this.lblErrors.Size = new System.Drawing.Size(37, 13);
+            this.lblErrors.TabIndex = 12;
+            this.lblErrors.Text = "Errors:";
+            // 
+            // lblExceptions
+            // 
+            this.lblExceptions.AutoSize = true;
+            this.lblExceptions.Location = new System.Drawing.Point(760, 74);
+            this.lblExceptions.Name = "lblExceptions";
+            this.lblExceptions.Size = new System.Drawing.Size(62, 13);
+            this.lblExceptions.TabIndex = 13;
+            this.lblExceptions.Text = "Exceptions:";
+            // 
+            // lblErrorsValue
+            // 
+            this.lblErrorsValue.AutoSize = true;
+            this.lblErrorsValue.Location = new System.Drawing.Point(871, 42);
+            this.lblErrorsValue.Name = "lblErrorsValue";
+            this.lblErrorsValue.Size = new System.Drawing.Size(35, 13);
+            this.lblErrorsValue.TabIndex = 14;
+            this.lblErrorsValue.Text = "label2";
+            // 
+            // lblExceptionsValue
+            // 
+            this.lblExceptionsValue.AutoSize = true;
+            this.lblExceptionsValue.Location = new System.Drawing.Point(871, 74);
+            this.lblExceptionsValue.Name = "lblExceptionsValue";
+            this.lblExceptionsValue.Size = new System.Drawing.Size(35, 13);
+            this.lblExceptionsValue.TabIndex = 15;
+            this.lblExceptionsValue.Text = "label3";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.фToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(930, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // фToolStripMenuItem
+            // 
+            this.фToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.параметрыToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.фToolStripMenuItem.Name = "фToolStripMenuItem";
+            this.фToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.фToolStripMenuItem.Text = "Файл";
+            // 
+            // параметрыToolStripMenuItem
+            // 
+            this.параметрыToolStripMenuItem.Name = "параметрыToolStripMenuItem";
+            this.параметрыToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.параметрыToolStripMenuItem.Text = "Параметры";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(930, 619);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.lblExceptionsValue);
+            this.Controls.Add(this.lblErrorsValue);
+            this.Controls.Add(this.lblExceptions);
+            this.Controls.Add(this.lblErrors);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnMessage;
+        private System.Windows.Forms.ColumnHeader columnDate;
+        private System.Windows.Forms.ColumnHeader columnType;
+        private System.Windows.Forms.ColumnHeader columnID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblErrors;
+        private System.Windows.Forms.Label lblExceptions;
+        private System.Windows.Forms.Label lblErrorsValue;
+        private System.Windows.Forms.Label lblExceptionsValue;
+        private System.Windows.Forms.ColumnHeader columnApplication;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem фToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem параметрыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ WinFormsTCP_Server/MainForm/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Globalization;
+
+using log4net;
+using log4net.Config;
+
+namespace WinFormsTCP_Server
+{
+    public partial class Form1 : Form
+    {
+        //private static ILog someLOg;
+        CreateLogClass cls = new CreateLogClass();
+        TcpListener listner = new TcpListener(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000));
+        public Form1()
+        {
+            InitializeComponent();
+
+            //this.Hide();
+            this.WindowState = FormWindowState.Minimized;
+
+            notifyIcon1.Visible = true;
+            lblErrorsValue.Text = "0";
+            lblExceptionsValue.Text = "0";
+            log4net.Config.DOMConfigurator.Configure();
+
+            listner.Start();
+            Thread myThread = new Thread(someFunc);
+            myThread.IsBackground = true;
+            myThread.Start();
+        }
+
+        public void someFunc()
+        {
+            int countErrors = 0, countExceptions = 0, listCounter = 1;
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false; // ненадежная фигня :/
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = listner.AcceptTcpClient();
+                    StreamReader sr = new StreamReader(client.GetStream());
+                    string varFromClient = sr.ReadLine();
+                    
+                    //--------------------------
+                    string varFromClientBegin = varFromClient.Remove(varFromClient.Length - 3);
+                    string varFromClientEnd = varFromClient.Remove(0,varFromClient.Length - 3);
+                    string varFromClient_onlyCommand = varFromClientBegin.Remove(varFromClientBegin.Length - 6);
+                    //--------------------------
+
+                    int index = listView1.Items.Add(listCounter.ToString()).Index; // Добавление ID в listView
+                    listView1.Items[index].SubItems.Add(varFromClient_onlyCommand); // Добавление Сообщения в listView 
+                    listView1.Items[index].SubItems.Add(DateTime.Now.ToString()); // Добавление времени в listView 
+                    listView1.Items[index].SubItems.Add(addedCondition(varFromClient, varFromClient_onlyCommand)); // Добавление Типа сообщения в listView 
+                    condition(varFromClientEnd, varFromClientBegin, index, ref countErrors, ref countExceptions);
+                    listView1.Items[index].SubItems.Add(ProgramCondition(varFromClientBegin)); // Добавление приложения в listView 
+                    ++listCounter;
+                    client.Close();
+                }
+                catch (System.IO.IOException io)
+                {
+                    MessageBox.Show("IOException:\n" + io.ToString());
+                }
+            }
+        }
+
+        //---------------------------Проверка условия----------------------------------------------
+
+        public void condition(string varFromClientEnd, string varFromClientBegin, int index, ref int countErrors, ref int countExceptions)
+        {
+            switch (varFromClientEnd)
+            {
+                case "ftl":
+                    lblErrorsValue.Text = countErrors.ToString();
+                    this.notifyIcon1.Icon = new Icon("..\\Debug\\icons\\delete1.ico");
+                    listView1.Items[index].BackColor = Color.Coral;
+
+                    notifyIcon1.BalloonTipIcon = ToolTipIcon.Error;
+                    notifyIcon1.BalloonTipTitle = "Fatal Error";
+                    notifyIcon1.BalloonTipText = "Fatal Error text: " + varFromClientBegin;
+                    notifyIcon1.ShowBalloonTip(4);
+                    break;
+                case "err":
+                    ++countErrors;
+                    lblErrorsValue.Text = countErrors.ToString();
+                    this.notifyIcon1.Icon = new Icon("..\\Debug\\icons\\delete1.ico");
+                    listView1.Items[index].BackColor = Color.Coral;
+
+                    //contextMenuStrip1.Show();
+                    notifyIcon1.BalloonTipIcon = ToolTipIcon.Error;
+                    notifyIcon1.BalloonTipTitle = "Error";
+                    notifyIcon1.BalloonTipText = "Error text: " + varFromClientBegin;
+                    notifyIcon1.ShowBalloonTip(4);
+                    break;
+                case "wrn":
+                    ++countExceptions;
+                    lblExceptionsValue.Text = countExceptions.ToString();
+                    this.notifyIcon1.Icon = new Icon("..\\Debug\\icons\\alert.ico");
+                    listView1.Items[index].BackColor = Color.SandyBrown;
+
+                    notifyIcon1.BalloonTipIcon = ToolTipIcon.Warning;
+                    notifyIcon1.BalloonTipTitle = "Exception";
+                    notifyIcon1.BalloonTipText = "Exception text: " + varFromClientBegin;
+                    notifyIcon1.ShowBalloonTip(4);
+                    break;
+                default:
+                    this.notifyIcon1.Icon = new Icon("..\\Debug\\icons\\success.ico");
+                    break;
+            }
+        }
+
+        //------------------------Проверка на тип-----------------------------
+        public string addedCondition(string varFromClient, string varFromClient_onlyCommand)
+        {
+            string endStr = varFromClient.Remove(0, varFromClient.Length - 3);
+            switch (endStr)
+            {
+                case "inf":
+                    cls.Info(varFromClient_onlyCommand);
+                    return "Info";
+                    break;
+                case "wrn":
+                    cls.Warning(varFromClient_onlyCommand);
+                    return "Warning";
+                    break;
+                case "err":
+                    cls.Error(varFromClient_onlyCommand);
+                    return "Error";
+                    break;
+                case "dbg":
+                    cls.Debug(varFromClient_onlyCommand);
+                    return "Debug";
+                    break;
+                case "ftl":
+                    cls.Fatal(varFromClient_onlyCommand);
+                    return "Fatal";
+                    break;
+                default:
+                    return "Unknown";
+                    break;
+            }
+        }
+        //-------------------------Определение программы-------------------------
+
+        public string ProgramCondition(string varFromClientBegin)
+        {
+            string ProgramTitle = varFromClientBegin.Remove(0, varFromClientBegin.Length - 6);
+
+            switch (ProgramTitle)
+            {
+                case "DcCore":
+                    return ProgramTitle;
+                    break;
+                case "cAdmin":
+                    ProgramTitle = "DcAdmin";
+                    return ProgramTitle;
+                    break;
+                default:
+                    ProgramTitle = "Unknown";
+                    return ProgramTitle;
+                    break;
+            }
+        }
+        //-----------------------------------------------------------------------
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Тестовая кнопка
+        {
+            //cls.Error("This is Error12122!");
+            //cls.Fatal("This is Fatal!");
+            //cls.Info("This is Info!");
+            //cls.Warning("This is Warning!");
+            //cls.Debug("This is Debug!");
+
+            if (this.WindowState == FormWindowState.Normal)
+                MessageBox.Show("It's normal");
+            else if (this.WindowState == FormWindowState.Minimized)
+                MessageBox.Show("It's minimized");
+            else if (this.WindowState == FormWindowState.Maximized)
+                MessageBox.Show("It's maximized");
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess();
+            MessageBox.Show(System.Diagnostics.Process.GetCurrentProcess().ToString());
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+
+    public class CreateLogClass
+    {
+        public static readonly ILog log = LogManager.GetLogger(typeof(CreateLogClass));
+
+        public void Error(string send_message)
+        {
+            log.Error(send_message);
+        }
+
+        public void Info(string send_message)
+        {
+            log.Info(send_message);
+        }
+
+        public void Debug(string send_message)
+        {
+            log.Debug(send_message);
+        }
+
+        public void Warning(string send_message)
+        {
+            log.Warn(send_message);
+        }
+
+        public void Fatal(string send_message)
+        {
+            log.Fatal(send_message);
+        }
+    }
+}
